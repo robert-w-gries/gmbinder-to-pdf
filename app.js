a27e27f285b8cd6a0f495b218c2d0015c5cc9e4d
Rob Gries <robert.w.gries@gmail.com>
2021-05-15 18:34:46 -0400

    Fix output pdf name

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ import puppeteer from "puppeteer";
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.gmbinder.com/share/-M1kp63806k2QswdeySI');
-  await page.pdf({ path: 'test.pdf', format: 'a4' });
+  await page.pdf({ path: 'spelljammers_logbook.pdf', format: 'a4' });
   await browser.close();
 })();
